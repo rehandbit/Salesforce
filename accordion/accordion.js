9843bf94fbe778dbfd4638942dfa3225fe079915
Rehan <63359688+rehandbit@users.noreply.github.com>
2023-10-21 16:01:35 +0530

    adding accordion

---
+++ accordion/accordion.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class Accordion extends LightningElement {
+    // section = '';
+
+    // handleClick(event) {
+    //     this.section = 'B';
+    // }
+
+    // handleSectionToggle(event) {
+    //     this.section = event.detail.openSections;
+    // }
+}
